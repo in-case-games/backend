5de70e29e4e3585cff4a4e9a0e08903a204c5c37
Ferbray <yt_ferbray@mail.ru>
2023-01-23 08:36:00 +0300

    Create wpf
    Create viewModel, view, locator, services

---
+++ CaseApplication.WPF/Service/PageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace CaseApplication.WPF.Service
+{
+    public class PageService
+    {
+        public event Action<Page>? OnPageChanged;
+        public void ChangePage(Page page) => OnPageChanged?.Invoke(page);
+    }
+}
---
+++ CaseApplication.WPF/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CaseApplication.WPF.Core;
+using CaseApplication.WPF.Service;
+using CaseApplication.WPF.View.StartUp;
+using System.Windows.Controls;
+
+namespace CaseApplication.WPF.ViewModel
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private readonly PageService _pageService;
+
+        private Page? pageSource;
+
+        public Page? PageSource { 
+            get => pageSource; 
+            set { 
+                pageSource = value;
+                OnPropertyChanged(nameof(PageSource));
+            } 
+        }
+
+        public MainWindowViewModel(PageService pageService)
+        {
+            _pageService = pageService;
+
+            _pageService.OnPageChanged += page => PageSource = page;
+            _pageService.ChangePage(new AuthorizationPage());
+        }
+    }
+}
