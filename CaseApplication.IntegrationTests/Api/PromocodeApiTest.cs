cc57672301039fe66ea5bc15dc7cf2d9a2fda292
Ferbray <yt_ferbray@mail.ru>
2023-01-30 17:11:00 +0300

    add promocode api test

--- CaseApplication.IntegrationTests/Api/PromocodeApiTest.cs
+++ CaseApplication.IntegrationTests/Api/PromocodeApiTest.cs
@@ -1,4 +1,5 @@
-﻿using CaseApplication.WebClient.Services;
+﻿using CaseApplication.DomainLayer.Entities;
+using CaseApplication.WebClient.Services;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace CaseApplication.IntegrationTests.Api
@@ -15,7 +16,29 @@ namespace CaseApplication.IntegrationTests.Api
         [Fact]
         public async Task PromocodeSimpleTests()
         {
+            //Create
+            Guid searchTypePromocodeId = (await _clientApi
+                .ResponseGet<PromocodeType>("/PromocodeType/GetByName?name=balance")).Id;
 
+            Promocode promocode = new() {
+                PromocodeDiscount = 10M,
+                PromocodeName = "Стандарт на пополнение",
+                PromocodeTypeId = searchTypePromocodeId, 
+                PromocodeUsesCount = 1000000000
+            };
+
+            await _clientApi.ResponsePost("/Promocode", promocode);
+
+            //Get
+            promocode = await _clientApi
+                .ResponseGet<Promocode>($"/Promocode/GetByName?name={promocode.PromocodeName}");
+
+            //Update
+            promocode.PromocodeUsesCount = 999999999;
+            await _clientApi.ResponsePut("/Promocode", promocode);
+
+            //Delete
+            await _clientApi.ResponseDelete($"/Promocode?id={promocode.Id}");
         }
     }
 }
