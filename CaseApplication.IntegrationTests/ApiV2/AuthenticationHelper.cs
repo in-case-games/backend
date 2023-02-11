245a1602832e4bbc5ba5c41616a4feee287e22c3
zmqp <81153204+prn-ic@users.noreply.github.com>
2023-02-11 14:49:11 +0500

    auth helper for api tests

---
+++ CaseApplication.IntegrationTests/ApiV2/AuthenticationHelper.cs
@@ -0,0 +1,57 @@
+﻿using Azure.Core;
+using CaseApplication.Api.Services;
+using CaseApplication.DomainLayer.Entities;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CaseApplication.IntegrationTests.ApiV2
+{
+    public class AuthenticationHelper
+    {
+        private  IConfiguration _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddUserSecrets<Program>()
+            .Build();
+        private readonly JwtHelper _jwtHelper;
+        public AuthenticationHelper()
+        {
+            _jwtHelper = new(_configuration);
+        }
+        public string CreateToken()
+        {
+            UserAdditionalInfo info = new UserAdditionalInfo
+            {
+                UserRole = new UserRole { RoleName = "admin" }
+            };
+            User user = new User
+            {
+                Id = Guid.NewGuid(),
+                UserLogin = "UserForTests",
+                UserEmail = "UserEmailForTest",
+                UserAdditionalInfo = info
+            };
+
+            Claim[] claims = GenerateClaimsForAccessToken(user);
+            TimeSpan expiration = TimeSpan.FromMinutes(5);
+
+            JwtSecurityToken token = _jwtHelper.CreateResuableToken(claims, expiration);
+                
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        private Claim[] GenerateClaimsForAccessToken(User user)
+        {
+            //Find future data for claims
+            Guid roleId = user.UserAdditionalInfo!.UserRoleId;
+
+            string roleName = user.UserAdditionalInfo.UserRole!.RoleName!;
+
+            return new Claim[] {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, roleName),
+                new Claim(ClaimTypes.Name, user.UserLogin!),
+                new Claim(ClaimTypes.Email, user.UserEmail!)
+            };
+        }
+    }
+}
---
+++ CaseApplication.IntegrationTests/ApiV2/UserApiTest.cs
@@ -0,0 +1,86 @@
+﻿using CaseApplication.Api.Controllers;
+using CaseApplication.DomainLayer.Entities;
+using CaseApplication.DomainLayer.Repositories;
+using CaseApplication.WebClient.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Moq;
+
+namespace CaseApplication.IntegrationTests.ApiV2
+{
+    public class UserApiTest: IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly string _accessToken;
+        private readonly AuthenticationHelper _helper = new();
+        private readonly ResponseHelper _response;
+        private static readonly Guid guid = Guid.NewGuid();
+        public UserApiTest(WebApplicationFactory<Program> app)
+        {
+            _response = new(app.CreateClient());
+            _accessToken = _helper.CreateToken();
+        }
+        [Fact]
+        public async Task GET_AllUsers_ShouldBeOk()
+        {
+            // Arrange
+            throw new Exception(_accessToken);
+            // Act
+
+            // Assert
+        }
+        [Fact]
+        public async Task GET_GetById_ShouldBeOk()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [Fact]
+        public async Task GET_GetByNonExistedId_ShouldBeNotFound()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [Fact]
+        public async Task GET_GetByLogin_ShouldBeOk()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [Fact]
+        public async Task GET_GetByNonExistedLogin_ShouldBeNotFound()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        private static List<User> InitializeUserLibrary()
+        {
+            var users = new List<User>();
+
+            UserAdditionalInfo info = new UserAdditionalInfo
+            {
+                UserRole = new UserRole { RoleName = "user" }
+            };
+            users.Add(new User
+            {
+                Id = guid,
+                UserLogin = "UserUserForTests1",
+                UserEmail = "UserEmailUserForTest1",
+                UserAdditionalInfo = info
+            });
+
+
+            return users;
+        }
+    }
+}
