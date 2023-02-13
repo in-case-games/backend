0198c3d969bd783ede218e9e1b751ef09d877f5a
Ferbray <yt_ferbray@mail.ru>
2023-02-13 15:31:10 +0300

    add new api test

---
+++ CaseApplication.IntegrationTests/ApiV2/GameItemApiTest.cs
@@ -0,0 +1,193 @@
+﻿using CaseApplication.DomainLayer.Entities;
+using CaseApplication.WebClient.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseApplication.IntegrationTests.ApiV2
+{
+    public class GameItemApiTest : IntegrationTestHelper, IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly ResponseHelper _response;
+
+        public GameItemApiTest(WebApplicationFactory<Program> app)
+        {
+            _response = new(app.CreateClient());
+        }
+
+        [Fact]
+        public async Task GET_GetItem_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            GameItem? gameItem = new()
+            {
+                GameItemCost = 1,
+                GameItemName = "CASEXY",
+                GameItemRarity = "XYT",
+                GameItemImage = "PICTURE"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode("/GameItem/admin", gameItem, AccessToken);
+            gameItem = await _response.ResponseGet<GameItem>($"/GameItem/name/{gameItem.GameItemName}");
+            HttpStatusCode statusCodeGet = await _response.ResponseGetStatusCode($"/GameItem/{gameItem!.Id}");
+            await _response.ResponseDelete($"/GameItem/admin/{gameItem!.Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeGet);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task GET_GetByNameItem_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            GameItem? gameItem = new() { 
+                GameItemCost = 1,
+                GameItemName = "CASEXY",
+                GameItemRarity = "XYT",
+                GameItemImage = "PICTURE"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode("/GameItem/admin", gameItem, AccessToken);
+            gameItem = await _response.ResponseGet<GameItem>($"/GameItem/name/{gameItem.GameItemName}");
+            HttpStatusCode statusCodeGet = gameItem is null ? 
+                HttpStatusCode.NotFound : HttpStatusCode.OK;
+            await _response.ResponseDelete($"/GameItem/admin/{gameItem!.Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeGet);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task GET_GetAllItem_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            GameItem? gameItem = new()
+            {
+                GameItemCost = 1,
+                GameItemName = "CASEXY",
+                GameItemRarity = "XYT",
+                GameItemImage = "PICTURE"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode("/GameItem/admin", gameItem, AccessToken);
+            gameItem = await _response.ResponseGet<GameItem>($"/GameItem/name/{gameItem.GameItemName}");
+            HttpStatusCode statusCodeGet = await _response.ResponseGetStatusCode($"/GameItem/all");
+            await _response.ResponseDelete($"/GameItem/admin/{gameItem!.Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeGet);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task POST_CreateItem_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            GameItem? gameItem = new()
+            {
+                GameItemCost = 1,
+                GameItemName = "CASEXY",
+                GameItemRarity = "XYT",
+                GameItemImage = "PICTURE"
+            };
+
+            //Act
+            HttpStatusCode statusCodeCreate = await _response.ResponsePostStatusCode(
+                "/GameItem/admin", gameItem, AccessToken);
+            gameItem = await _response.ResponseGet<GameItem>($"/GameItem/name/{gameItem.GameItemName}");
+            await _response.ResponseDelete($"/GameItem/admin/{gameItem!.Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeCreate);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task PUT_UpdateItem_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            GameItem? gameItem = new()
+            {
+                GameItemCost = 1,
+                GameItemName = "CASEXY",
+                GameItemRarity = "XYT",
+                GameItemImage = "PICTURE"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode(
+                "/GameItem/admin", gameItem, AccessToken);
+            gameItem = await _response.ResponseGet<GameItem>($"/GameItem/name/{gameItem.GameItemName}");
+            gameItem!.GameItemRarity = "XTI";
+            HttpStatusCode statusCodeUpdate = await _response.ResponsePut(
+                $"/GameItem/admin", gameItem, AccessToken);
+            await _response.ResponseDelete($"/GameItem/admin/{gameItem!.Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeUpdate);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task DELETE_DeleteItem_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            GameItem? gameItem = new()
+            {
+                GameItemCost = 1,
+                GameItemName = "CASEXY",
+                GameItemRarity = "XYT",
+                GameItemImage = "PICTURE"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode("/GameItem/admin", gameItem, AccessToken);
+            gameItem = await _response.ResponseGet<GameItem>($"/GameItem/name/{gameItem.GameItemName}");
+            HttpStatusCode statusCodeDelete = await _response.ResponseDelete(
+                $"/GameItem/admin/{gameItem!.Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeDelete);
+
+            await RemoveTestUser(guid);
+        }
+    }
+}
---
+++ CaseApplication.IntegrationTests/ApiV2/NewsApiTest.cs
@@ -0,0 +1,153 @@
+﻿using CaseApplication.DomainLayer.Entities;
+using CaseApplication.WebClient.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace CaseApplication.IntegrationTests.ApiV2
+{
+    public class NewsApiTest : IntegrationTestHelper, IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly ResponseHelper _response;
+
+        public NewsApiTest(WebApplicationFactory<Program> app)
+        {
+            _response = new(app.CreateClient());
+        }
+
+        [Fact]
+        public async Task GET_GetNews_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            News news = new()
+            {
+                NewsName = "NEW UPDATE",
+                NewsContent = "YRA YRA"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode("/News/admin", news, AccessToken);
+            List<News> listNews = (await _response.ResponseGet<List<News>>("/News/all"))!;
+            HttpStatusCode statusCodeGet = await _response.ResponseGetStatusCode(
+                $"/News/{listNews[0].Id}");
+            await _response.ResponseDelete($"/News/admin/{listNews[0].Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeGet);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task GET_GetAllNews_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            News news = new()
+            {
+                NewsName = "NEW UPDATE",
+                NewsContent = "YRA YRA"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode("/News/admin", news, AccessToken);
+            List<News> listNews = (await _response.ResponseGet<List<News>>("/News/all"))!;
+            HttpStatusCode statusCodeGet = listNews.Count > 0 ? 
+                HttpStatusCode.OK : HttpStatusCode.NotFound;
+            await _response.ResponseDelete($"/News/admin/{listNews[0].Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeGet);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task POST_CreateNews_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            News news = new()
+            {
+                NewsName = "NEW UPDATE",
+                NewsContent = "YRA YRA"
+            };
+
+            //Act
+            HttpStatusCode statusCodeCreate = await _response.ResponsePostStatusCode(
+                "/News/admin", news, AccessToken);
+            List<News> listNews = (await _response.ResponseGet<List<News>>("/News/all"))!;
+            await _response.ResponseDelete($"/News/admin/{listNews[0].Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeCreate);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task PUT_UpdateNews_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            News news = new()
+            {
+                NewsName = "NEW UPDATE",
+                NewsContent = "YRA YRA"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode(
+                "/News/admin", news, AccessToken);
+            List<News> listNews = (await _response.ResponseGet<List<News>>("/News/all"))!;
+            listNews[0].NewsName = "NEW UPDATES";
+            HttpStatusCode statusCodeUpdate = await _response.ResponsePut(
+                "/News/admin", listNews[0], AccessToken);
+            await _response.ResponseDelete($"/News/admin/{listNews[0].Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeUpdate);
+
+            await RemoveTestUser(guid);
+        }
+
+        [Fact]
+        public async Task DELETE_DeleteNews_ReturnsOk()
+        {
+            //Arrange
+            Guid guid = Guid.NewGuid();
+
+            await InitializeTestUser(guid);
+
+            News news = new()
+            {
+                NewsName = "NEW UPDATE",
+                NewsContent = "YRA YRA"
+            };
+
+            //Act
+            await _response.ResponsePostStatusCode(
+                "/News/admin", news, AccessToken);
+            List<News> listNews = (await _response.ResponseGet<List<News>>("/News/all"))!;
+            HttpStatusCode statusCodeDelete = await _response.ResponseDelete(
+                $"/News/admin/{listNews[0].Id}", AccessToken);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, statusCodeDelete);
+
+            await RemoveTestUser(guid);
+        }
+    }
+}
