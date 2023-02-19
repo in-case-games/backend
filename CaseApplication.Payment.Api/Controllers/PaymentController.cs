104ffee71741320a30823679e08a4ca574b13d37
Ferbray <yt_ferbray@mail.ru>
2023-02-19 14:40:59 +0300

    add payment maket

---
+++ CaseApplication.Payment.Api/Controllers/PaymentController.cs
@@ -0,0 +1,21 @@
+﻿using CaseApplication.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace CaseApplication.Payment.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
+        private Guid UserId => Guid
+            .Parse(User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+        public PaymentController(IDbContextFactory<ApplicationDbContext> contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+    }
+}
--- CaseApplication.Payment.Api/Program.cs
+++ CaseApplication.Payment.Api/Program.cs
@@ -1,15 +1,83 @@
+using CaseApplication.Infrastructure.Data;
+using CaseApplication.Infrastructure.Helpers;
+using CaseApplication.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
+builder.Services.AddDbContextFactory<ApplicationDbContext>(
+    options => options.UseSqlServer(
+#if DEBUG
+        builder.Configuration["ConnectionStrings:DevelopmentConnection"]
+#else
+        builder.Configuration["ConnectionStrings:ProductionConnection"],
+#endif
+        )
+);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"]!,
+
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JWT:ValidAudience"]!,
+
+            ValidateLifetime = true,
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]!)),
+
+            ValidateIssuerSigningKey = true,
+        };
+    });
+
+builder.Services.AddSingleton<EmailService>();
+builder.Services.AddSingleton<EmailHelper>();
+
+builder.Services.AddSingleton<EncryptorHelper>();
+builder.Services.AddSingleton<JwtHelper>();
+builder.Services.AddSingleton<ValidationService>();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Uebanization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your dick is fall in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,6 +86,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
