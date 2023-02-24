2d07637e098d91f2ab47cfc0d8dbdd833e1a7c2e
zmqp <81153204+prn-ic@users.noreply.github.com>
2023-02-24 08:50:36 +0500

    refactor

--- CaseApplication.Domain/Dtos/UserHistoryOpeningCasesDto.cs
+++ CaseApplication.Domain/Dtos/UserHistoryOpeningCasesDto.cs
@@ -1,12 +1,17 @@
 ﻿using CaseApplication.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace CaseApplication.Domain.Dtos
 {
     public class UserHistoryOpeningCasesDto: BaseEntity
     {
+        [Required]
         public Guid UserId { get; set; }
+        [Required]
         public Guid GameItemId { get; set; }
+        [Required]
         public Guid GameCaseId { get; set; }
+        [Required]
         public DateTime? CaseOpenAt { get; set; }
     }
 }
