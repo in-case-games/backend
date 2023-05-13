b53d870c1d29c0243814284e1b2a6d1ff2fd36f0
zmqp <domarlaid2@mail.ru>
2023-05-13 12:47:57 +0500

    update this

---
+++ InCase/Services/Resources/DAL/EntityFramework/Models/Payment/BalanceMarket.cs
@@ -0,0 +1,8 @@
+﻿namespace InCase.Domain.Entities.Payment
+{
+    public class BalanceMarket
+    {
+        public decimal Balance { get; set; }
+        public string? Result { get; set; }
+    }
+}
