9d6aa4bce1ee00272e9c7552cdb751e9bafdd9e3
Ferbray <yt_ferbray@mail.ru>
2023-05-19 13:15:48 +0300

    создал интерфейсы инвентаря контроллера

---
+++ InCase/Services/Withdraw/Withdraw.BLL/Interfaces/IUserInventoryService.cs
@@ -0,0 +1,14 @@
+﻿using Withdraw.BLL.Models;
+
+namespace Withdraw.BLL.Interfaces
+{
+    public interface IUserInventoryService
+    {
+        public Task<UserInventoryResponse> GetById(Guid id);
+        public Task<List<UserInventoryResponse>> Get(Guid userId);
+        public Task<List<UserInventoryResponse>> Get(Guid userId, int count);
+        public Task<decimal> Sell(Guid id, Guid userId);
+        public Task<decimal> SellLast(Guid itemId, Guid userId);
+        public Task<UserInventoryResponse> Exchange(Guid id, Guid itemId, Guid userId);
+    }
+}
---
+++ InCase/Services/Withdraw/Withdraw.BLL/Services/UserInventoryService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Withdraw.BLL.Exceptions;
+using Withdraw.BLL.Helpers;
+using Withdraw.BLL.Interfaces;
+using Withdraw.BLL.Models;
+using Withdraw.DAL.Data;
+using Withdraw.DAL.Entities;
+
+namespace Withdraw.BLL.Services
+{
+    public class UserInventoryService : IUserInventoryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserInventoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserInventoryResponse>> Get(Guid userId)
+        {
+            if (!await _context.Users.AnyAsync(u => u.Id == userId))
+                throw new NotFoundException("Пользователь не найден");
+
+            List<UserInventory> inventories = await _context.UserInventories
+                .AsNoTracking()
+                .Where(ui => ui.UserId == userId)
+                .ToListAsync();
+
+            return inventories.ToResponse();
+        }
+
+        public Task<List<UserInventoryResponse>> Get(Guid userId, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInventoryResponse> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInventoryResponse> Exchange(Guid id, Guid itemId, Guid userId)
+        {
+            throw new NotImplementedException();
+
+        }
+        public Task<decimal> Sell(Guid id, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<decimal> SellLast(Guid itemId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
