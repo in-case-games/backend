3f468d13d1f4c4f907b7e28c9d2301cf26d4c5a8
prnic <domarlaid2@mail.ru>
2023-08-12 13:20:48 +0500

    make task (update image\user additional info)

--- src/InCase/Services/Identity/Identity.BLL/Interfaces/IUserAdditionalInfoService.cs
+++ src/InCase/Services/Identity/Identity.BLL/Interfaces/IUserAdditionalInfoService.cs
@@ -1,4 +1,5 @@
 using Identity.BLL.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace Identity.BLL.Interfaces
 {
@@ -8,6 +9,6 @@ namespace Identity.BLL.Interfaces
         public Task<UserAdditionalInfoResponse> GetByUserIdAsync(Guid userId);
         public Task<UserAdditionalInfoResponse> UpdateRoleAsync(Guid userId, Guid roleId);
         public Task<UserAdditionalInfoResponse> UpdateDeletionDateAsync(Guid userId, DateTime? deletionDate);
-        public Task<UserAdditionalInfoResponse> UpdateImageAsync(Guid userId, string uri);
+        public Task<UserAdditionalInfoResponse> UpdateImageAsync(Guid userId, IFormFile image);
     }
 }
