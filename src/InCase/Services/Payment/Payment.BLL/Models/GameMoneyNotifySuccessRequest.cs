7eeb17334bc3bef072c4397017d49a874d832446
prnic <domarlaid2@mail.ru>
2023-08-12 16:06:29 +0500

    make todo (send { success: true; } to payment-service)

---
+++ src/InCase/Services/Payment/Payment.BLL/Models/GameMoneyNotifySuccessRequest.cs
@@ -0,0 +1,16 @@
+﻿using Payment.BLL.Interfaces;
+using System.Text.Json.Serialization;
+
+namespace Payment.BLL.Models
+{
+    public class GameMoneyNotifySuccessRequest : IGameMoneyRequest
+    {
+        [JsonPropertyName("success")] public bool Success { get; set; }
+        public string? SignatureHMAC { get; set; }
+
+        public override string ToString()
+        {
+            return $"success: {Success};";
+        }
+    }
+}
